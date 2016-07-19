0cd8762846b254bedd2e468241e8803ff343a96b
Joshua Smock <joshuasmock@gmail.com>
2016-07-19 02:26:47 -0700

    Remove warnAboutIncorrectUsage

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.patch = exports.warnAboutIncorrectUsage = exports.AppContainer = undefined;
+exports.patch = exports.AppContainer = undefined;
 
 var _AppContainer = require('./AppContainer');
 
 var _AppContainer2 = _interopRequireDefault(_AppContainer);
-
-var _warnAboutIncorrectUsage = require('./warnAboutIncorrectUsage');
-
-var _warnAboutIncorrectUsage2 = _interopRequireDefault(_warnAboutIncorrectUsage);
 
 var _patch = require('./patch');
 
@@ -20,5 +16,4 @@ var _patch2 = _interopRequireDefault(_patch);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.AppContainer = _AppContainer2.default;
-exports.warnAboutIncorrectUsage = _warnAboutIncorrectUsage2.default;
 exports.patch = _patch2.default;
