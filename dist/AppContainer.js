4d1107d3835e85181b935c0f739cbf661dc9a238
Joshua Smock <joshuasmock@gmail.com>
2016-09-30 23:01:38 -0700

    Update dist

--- dist/AppContainer.js
+++ dist/AppContainer.js
@@ -1,30 +1,17 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactDeepForceUpdate = require('react-deep-force-update');
-
-var _reactDeepForceUpdate2 = _interopRequireDefault(_reactDeepForceUpdate);
-
-var _redboxReact = require('redbox-react');
-
-var _redboxReact2 = _interopRequireDefault(_redboxReact);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var React = require('react');
+var deepForceUpdate = require('react-deep-force-update');
+var Redbox = require('redbox-react').default;
+var Component = React.Component;
 
 var AppContainer = function (_Component) {
   _inherits(AppContainer, _Component);
@@ -55,7 +42,7 @@ var AppContainer = function (_Component) {
       });
       // Force-update the whole tree, including
       // components that refuse to update.
-      (0, _reactDeepForceUpdate2.default)(this);
+      deepForceUpdate(this);
     }
 
     // This hook is going to become official in React 15.x.
@@ -77,22 +64,19 @@ var AppContainer = function (_Component) {
       var error = this.state.error;
 
       if (error) {
-        return _react2.default.createElement(this.props.errorReporter, { error: error });
+        return React.createElement(this.props.errorReporter, { error: error });
       }
 
       if (this.props.component) {
-        return _react2.default.createElement(this.props.component, this.props.props);
+        return React.createElement(this.props.component, this.props.props);
       }
 
-      return _react2.default.Children.only(this.props.children);
+      return React.Children.only(this.props.children);
     }
   }]);
 
   return AppContainer;
-}(_react.Component);
-
-exports.default = AppContainer;
-
+}(Component);
 
 AppContainer.propTypes = {
   component: function component(props) {
@@ -110,7 +94,7 @@ AppContainer.propTypes = {
     return undefined;
   },
   children: function children(props) {
-    if (_react2.default.Children.count(props.children) !== 1) {
+    if (React.Children.count(props.children) !== 1) {
       return new Error('Invalid prop "children" supplied to AppContainer. ' + 'Expected a single React element with your app’s root component, e.g. <App />.');
     }
 
@@ -119,5 +103,7 @@ AppContainer.propTypes = {
 };
 
 AppContainer.defaultProps = {
-  errorReporter: _redboxReact2.default
+  errorReporter: Redbox
 };
+
+module.exports = AppContainer;
