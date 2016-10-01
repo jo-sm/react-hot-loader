1cc1f6c67495a194c2bcdf22edb115ef7c443064
Joshua Smock <joshuasmock@gmail.com>
2016-09-30 23:30:35 -0700

    Fix patch.js

--- dist/patch.js
+++ dist/patch.js
@@ -5,7 +5,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var React = require('react');
-var createProxy = require('react-proxy').default;
+var createProxy = require('react-proxy');
 var global = require('global');
 
 var ComponentMap = function () {
